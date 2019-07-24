5dffdfdbb15ae24927a02a11fa0298e22dbee894
Colby Allen <colbyallen012@gmail.com>
2019-07-23 23:31:34 -0600

    move api key

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,5 +1,5 @@
   // const url = 'http://localhost:3000'
-import { apiKey } from '../apiKey'
+import { apiKey } from './apiKey'
 
 export const fetchMovies = () => {
   return fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}edd&language=en-US&page=1`)
