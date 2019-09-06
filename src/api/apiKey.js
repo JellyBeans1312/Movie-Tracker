fcde2823bbea21580426b3836ecdd912d62c4a78
Aidan McKay <aidanmckay2000@gmail.com>
2019-09-06 12:14:10 -0600

    rebase

--- src/api/apiKey.js
+++
@@ -1,1 +0,0 @@
-export const apiKey = '044b86cc1259591cb5a872bda8d25';
--- src/reducers/movieReducer.js
+++
@@ -1,9 +0,0 @@
-export const movieReducer = (state = [], action) => {
-  switch(action.type) {
-    case 'SHOW_MOVIES':
-      return action.movies;
-
-    default:
-      return state;
-  }
-}
